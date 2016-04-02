64edef1f300482c2bf3bfde1f88f110c9f1487b9
Mario Uzae <mariouzae@gmail.com>
2016-04-02 14:37:45 -0300

    Initial commit

---
+++ src/main/java/br/com/mackenzie/lp3/rentIt/RentItApplication.java
@@ -0,0 +1,12 @@
+package br.com.mackenzie.lp3.rentIt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RentItApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RentItApplication.class, args);
+	}
+}
---
+++ src/test/java/br/com/mackenzie/lp3/rentIt/RentItApplicationTests.java
@@ -0,0 +1,16 @@
+package br.com.mackenzie.lp3.rentIt;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = RentItApplication.class)
+public class RentItApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
